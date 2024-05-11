250b9dd49a216e017173d6f5140b1cc364efd319
Yaro Kulchyckyj <47095141+Yarchykk@users.noreply.github.com>
2024-05-10 23:53:29 -0400

    Finished coin from brackeys tutorial

--- BrackeysGodotBeginnerTutorial/scripts/coin.gd
+++ BrackeysGodotBeginnerTutorial/scripts/coin.gd
@@ -1,11 +1,7 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 Coin - this is just a print statment with no functionality")
+	queue_free()
